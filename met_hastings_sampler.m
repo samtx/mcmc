e3ea10aa380d2229800210a6f46b4d4023ea2e45
Sam Friedman <samtx@outlook.com>
2015-09-29 18:35:42 -0500

    fix normal pdf function

--- met_hastings_sampler.m
+++ met_hastings_sampler.m
@@ -5,11 +5,12 @@
 % Function for Cauchy distribution
 cauchy = @(x) 1./(1+x.^2);
 % Function for normal PDF
-norm = @(x,mu,sig) 1/sig*exp(-.5*(x-mu)/sig.^2);
+%norm = @(x,mu,sig) 1/sig*exp(-.5*(x-mu)/sig.^2);
+norm = @(x,mu,sig) normpdf(x,mu,sig);
 % Generate n samples in the chain
 n = 1e4;
 sig = 2;
-x = zeros(1,n);
+x = zeros(1,n);yy = zeros(1,n);
 x(1) = randn(1); % generate starting point
 % note that randn() returns a pseudorandom value drawn from the standard
 % normal distribution.
@@ -18,6 +19,7 @@ for i = 2:n
    % distribution in this case). This will be a normal with mean given by
    % the previous value in the chain and standard deviation of sig
    y = x(i-1) + sig*randn(1);
+   yy(i) = y;
    % generate a uniform for comparison
    u = rand(1);
    % note that rand() returns a pseudorandom value drawn from the standard
@@ -32,6 +34,9 @@ for i = 2:n
 end
 % Discard initial burn in points
 burn_in = 500;
-bin_vals = [-20:2:20];
+bin_vals = -20:2:20;
+x = x(burn_in+1:end);
 plot(x')
+hist(x,bin_vals);
+
 
